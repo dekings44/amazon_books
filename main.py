9cd15ebad0137147809c1c55e395bc206ecf40f3
Kingsley Ihemere <dekings44@gmail.com>
2022-09-02 14:40:41 +0100

    scraping amazon best seller

--- main.py
+++ main.py
@@ -11,9 +11,6 @@ import pandas as pd
 
 header = {'User-Agent':'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_13_6) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/71.0.3578.98 Safari/537.36'}
 
-
-headers = {'User-Agent':'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_13_6) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/71.0.3578.98 Safari/537.36'}
-
 #declaring the list of empty variables, So that we can append the data overall
 
 book_title = []
@@ -23,24 +20,26 @@ book_price = []
 
 
 #creating an array of values and passing it in the url for dynamic webpages
-pages = np.arange(1,50,1)
+pages = np.arange(1,5,1)
 
 #the whole core of the script
 for page in pages:
     page = requests.get("https://www.amazon.co.uk/best-sellers-books-Amazon/zgbs/books/ref=zg_bs_pg_1?_encoding=UTF8&pg="+str(page), headers=header)
     soup = BeautifulSoup(page.text, 'html.parser')
-    acc_data = soup.find_all('div', {id : 'gridItemRoot'})
+    acc_data = soup.find_all('div', {'id' : 'gridItemRoot'})
     sleep(randint(2,6))
+
+    print(len(acc_data))
     # print(acc_data[0])
-    for store in acc_data:
-        name = store.find('h3', {'class' : 's-item__title'}).text
-        acc_name.append(name)
+    # for store in acc_data:
+    #     name = store.find('h3', {'class' : 's-item__title'}).text
+    #     acc_name.append(name)
         
-        state = store.find('span', {'class' : 'SECONDARY_INFO'}).text
-        lap_state.append(state)
+    #     state = store.find('span', {'class' : 'SECONDARY_INFO'}).text
+    #     lap_state.append(state)
         
-        price = store.find('span', {'class' : 's-item__price'}).text
-        lap_price.append(price)
+    #     price = store.find('span', {'class' : 's-item__price'}).text
+    #     lap_price.append(price)
         
         
 
